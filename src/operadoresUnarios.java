83674bd5cdeff00bcc28f8eabeba00638ad9d0eb
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2025-06-07 08:27:31 -0300

    .

---
+++ src/operadoresUnarios.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class operadoresUnarios {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Coloque um numero da tabuada");
+        int numero1 = teclado.nextInt();
+
+
+        for (int j = 1; j <= numero1; j++) {
+            System.out.println("\n");
+
+            for (int i = 1; i <= 10; i++) {
+                int total = i*j;
+                System.out.println(j + " x " + i + " = " +total);
+            }
+        }
+
+    }
+}

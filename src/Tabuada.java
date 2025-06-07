081d5b258f380c0a8ae33df15fb3b53dbfe8a220
Mateus Saullo Felizardo <mateusaullo13@gmail.com>
2025-06-07 08:31:51 -0300

    .

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class operadoresUnarios {
+public class Tabuada {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
